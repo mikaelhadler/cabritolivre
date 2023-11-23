da4852aeff1e52a7b21807bbc471fa2980240210
Mikael Hadler <mhadler@lendingclub.com>
2023-11-23 15:22:59 -0300

    init files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <img src="https://media.makeameme.org/created/it-works-hallelujah.jpg" alt="it works hallelujah" />
+  )
+}
+
+export default App
