ebb4771db4121025ea686b5f12c176402d6afb7f
Mikael <mikaelhadler@gmail.com>
2023-11-23 16:21:06 -0300

    adjust form

--- src/components/NetlifyForm/index.jsx
+++ src/components/NetlifyForm/index.jsx
@@ -1,18 +1,48 @@
 const NetlifyForm = () => (
-  <form name="contact" netlify>
-    <p>
+  <form
+    name="contact"
+    method="post"
+    data-netlify="true"
+    onSubmit="submit"
+    data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="form-name" value="contact" />
+
+    <div hidden>
+      <input name="bot-field" />
+    </div>
+
+    <div>
       <label>
-        Name <input type="text" name="name" />
+        First name
+        <br />
+        <input type="text" name="first-name" />
       </label>
-    </p>
-    <p>
+    </div>
+
+    <div>
       <label>
-        Email <input type="email" name="email" />
+        Last name
+        <br />
+        <input type="text" name="last-name" />
       </label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
+    </div>
+
+    <div>
+      <label htmlFor="email">Email</label>
+      <br />
+      <input id="email" type="email" name="email" />
+    </div>
+
+    <div>
+      <label>
+        Any Comments?
+        <br />
+        <textarea name="comments"></textarea>
+      </label>
+    </div>
+
+    <button type="submit">Submit The Results</button>
   </form>
 );
 export default NetlifyForm;
